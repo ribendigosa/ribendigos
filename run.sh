51cab36ebf934cc0daa56fba96437f91bafb2cef
ribendigosa <93442808+ribendigosa@users.noreply.github.com>
2021-10-30 23:15:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,4 +17,5 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 #
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FAxSlowmo -p x -t 2
 
